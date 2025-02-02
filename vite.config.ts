dc191295c33dcc74e507a0cc0cb0c4842a17bb0f
BomEllen <jjjj200100@gmail.com>
2025-02-02 18:56:45 +0900

    [2/2] 학습 현황 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,39 @@
-import { defineConfig } from 'vite';
+import { type ConfigEnv, defineConfig } from 'vite';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-const viteConfig = defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic',
-    }),
-  ],
-  server: {
-    host: 'localhost',
-    port: 3000,
-  },
-  preview: {
-    host: 'localhost',
-    port: 8080,
-  },
-  css: {
-    devSourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+const viteConfig = defineConfig((env: ConfigEnv) => {
+  const idDevMode = env.mode.includes('development');
+  
+  return {
+    plugins: [
+      react({
+        jsxRuntime: 'automatic',
+      }),
+      tailwindcss(),
+    ],
+    server: {
+      host: 'localhost',
+      port: 3000,
     },
-  },
+    preview: {
+      host: 'localhost',
+      port: 8080,
+    },
+    css: {
+      devSourcemap: true,
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName: idDevMode ? '_[local]_-_[hash:base64:3]' : '_[hash:base64:6]_'
+      },
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
 });
 
 export default viteConfig;
