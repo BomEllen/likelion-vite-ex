c51d80d190cdd370cfacdb054a4050cbd3014e28
BomEllen <jjjj200100@gmail.com>
2025-02-15 16:31:28 +0900

    [2/13] 수업 정리

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,11 @@
+import MemoryComponentDataPage from '@/pages/memory-component-data';
+import AnimationWithMotionPage from '@/pages/animation-with-motion';
 import StateManagement from '@/pages/state-management';
 import ErrorBoundaryDemo from '@/pages/error-boundary';
+import DataFetchingPage from '@/pages/data-fetching';
 import SideEffectDemo from '@/pages/side-effect';
 import SearchListPage from '@/pages/search-list';
+import AccessDOMPage from '@/pages/access-dom';
 import LifecycleDemo from '@/pages/lifecycle';
 import TicTacToe from '@/pages/tic-tac-toe';
 import SignInForm from '@/pages/sign-in';
@@ -12,12 +16,40 @@ import NotFound from '@/pages/not-found';
 // 내비게이션 리스트 데이터
 
 export const navList = [
+  {
+    path: 'data-fetching',
+    text: '데이터 쿼리/뮤테이션',
+    element: <DataFetchingPage />,
+  },
+  {
+    path: 'animation',
+    text: '모션 애니메이션',
+    element: <AnimationWithMotionPage />,
+  },
+  {
+    path: 'accessing-dom',
+    text: 'DOM 접근/조작',
+    element: <AccessDOMPage />,
+  },
+  {
+    path: 'memory-component-data',
+    text: '데이터 메모리',
+    element: <MemoryComponentDataPage />,
+  },
   { path: 'search-list', text: '검색 리스트', element: <SearchListPage /> },
   { path: 'side-effect', text: '이펙트 처리', element: <SideEffectDemo /> },
-  { path: 'error-boundary', text: '에러 바운더리', element: <ErrorBoundaryDemo /> },
+  {
+    path: 'error-boundary',
+    text: '에러 바운더리',
+    element: <ErrorBoundaryDemo />,
+  },
   { path: 'lifecycles', text: '라이프 사이클', element: <LifecycleDemo /> },
   { path: 'tic-tac-toe', text: '틱택토', element: <TicTacToe /> },
-  { path: 'state-management', text: '상태 관리 전략', element: <StateManagement /> },
+  {
+    path: 'state-management',
+    text: '상태 관리 전략',
+    element: <StateManagement />,
+  },
   { path: 'signup', text: '회원가입', element: <SignUpForm /> },
   { path: 'signin', text: '로그인', element: <SignInForm /> },
 ];
